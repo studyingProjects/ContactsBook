29d9068f284e576a0e7db645c2b55e9ed4cc856a
studyingProjects <148430835+studyingProjects@users.noreply.github.com>
2024-02-11 12:33:14 +0300

    add TableView, add DataSource delegation

--- ContactsBook/ViewController.swift
+++ ContactsBook/ViewController.swift
@@ -17,3 +17,35 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        var cell: UITableViewCell
+        
+        if let reuseCell = tableView .dequeueReusableCell(withIdentifier: "contactCellIdentifier") {
+            print("Используем старую ячейку для строки с индексом \(indexPath.row)")
+            cell = reuseCell
+        } else {
+            print("Создаем новую ячейку для строки с индексом \(indexPath.row)")
+            cell = UITableViewCell(style: .default, reuseIdentifier: "contactCellIdentifier")
+        }
+        configure(cell: &cell, indexPath: indexPath)
+        
+        return cell
+    }
+        
+    func configure(cell: inout UITableViewCell, indexPath: IndexPath) {
+        var configuration = cell.defaultContentConfiguration()
+        configuration.text = "String \(indexPath.row)"
+        cell.contentConfiguration = configuration
+//        if #available(iOS 14, *) {
+//        var configuration = cell.defaultContentConfiguration() configuration.text = "Строка \(indexPath.row)" cell.contentConfiguration = configuration
+//        }
+    }
+    
+    
+}
