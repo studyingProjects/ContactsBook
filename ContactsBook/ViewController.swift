a7e2b093859f549df848dee5b74b22f8e6c766e0
studyingProjects <148430835+studyingProjects@users.noreply.github.com>
2024-02-11 10:42:15 +0300

    Initial Commit

---
+++ ContactsBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactsBook
+//
+//  Created by Andrei Shpartou on 11/02/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
